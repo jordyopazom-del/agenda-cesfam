621211090f3c2ac5c131500290e674099ee5a747
jordyopazom-del <jordyopazom@gmail.com>
2026-01-01 14:24:38 -0300

    Add files via upload

---
+++ logo_data.js
@@ -0,0 +1,3 @@
+// Logo Data (Placeholder)
+// Replace the string below with your base64 encoded image
+const LOGO_BASE64 = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+P+/HgAFhAJ/wlseKgAAAABJRU5ErkJggg==";
